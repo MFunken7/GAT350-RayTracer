d4d0853d560338eef5a249ea895911168411849c
Michael <114317916+MFunken7@users.noreply.github.com>
2023-12-05 11:55:56 -0700

    anti aliasing and tracer depth added

--- Source/RayTracer/Main.cpp
+++ Source/RayTracer/Main.cpp
@@ -1,5 +1,9 @@
 #include "Renderer.h"
 #include "Random.h"
+#include "Camera.h"
+#include "Scene.h"
+#include "Material.h"
+#include "Sphere.h"
 #include <iostream>
 
 int main(int argc, char* argv[]) {
@@ -7,12 +11,40 @@ int main(int argc, char* argv[]) {
 
 	Renderer renderer = Renderer();
 	renderer.Initialize();
-	renderer.CreateWindow("RayTracing", 400, 300);
-	Canvas canvas(400, 300, renderer);
+	renderer.CreateWindow("RayTracing", 800, 600);
+	Canvas canvas(800, 600, renderer);
+
+	float aspectRatio = canvas.GetSize().x / (float)canvas.GetSize().y;
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 0, 1 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 90.0f, aspectRatio);
+
+	Scene scene(20); // sky color could be set with the top and bottom color
+	scene.SetCamera(camera);
+
+	// create material
+	auto lambertian = std::make_shared<Lambertian>(color3_t{ 0, 0, 1 });
+	auto metal = std::make_shared<Metal>(color3_t{ 1, 1, 1 }, 0.0f);
+
+
+	// create objects -> add to scene
+	srand(time(NULL));
+	// create objects -> add to scene
+	for (int i = 0; i < 10; i++)
+	{
+		std::shared_ptr<Material> material = (rand() % 2) ? std::dynamic_pointer_cast<Material>(lambertian) : std::dynamic_pointer_cast<Material>(metal);
+		auto sphere = std::make_unique<Sphere>(random(glm::vec3{ -1 }, glm::vec3{ 1 }), random(0.1f, 0.5f), material);
+		scene.AddObject(std::move(sphere));
+	}
+
+	canvas.Clear({ 0, 0, 0, 1 });
+	//for (int i = 0; i < 1000; i++) canvas.DrawPoint({ random01() * 400, random01() * 300 }, { random01(),random01(),random01(), 1 });
+	scene.Render(canvas, 50);
+	canvas.Update();
 
 	bool quit = false;
 	while (!quit)
 	{
+		
+
 		SDL_Event event;
 		SDL_PollEvent(&event);
 		switch (event.type)
@@ -30,11 +62,11 @@ int main(int argc, char* argv[]) {
 			break;
 		}
 
-		canvas.Clear({ 0, 0, 0, 1 });
-		for (int i = 0; i < 1000; i++) canvas.DrawPoint({ random01() * 400, random01() * 300 }, { random01(),random01(),random01(), 1 });
-		canvas.Update();
+		
 
 		renderer.PresentCanvas(canvas);
+
+		
 	}
 
 	renderer.Shutdown();
--- Source/RayTracer/MathUtils.h
+++ Source/RayTracer/MathUtils.h
@@ -1,8 +1,32 @@
 #pragma once
+#include <glm/glm.hpp>
 
 
 	template <typename T>
 	inline T lerp(const T& a, const T& b, float t)
 	{
-		lerp(a, b, t) = (a * (1 - t)) + (t * b);
+		return (a * (1 - t)) + (t * b);
+	}
+
+	inline glm::vec3 cross(const glm::vec3& v1, const glm::vec3& v2)
+	{
+		return glm::vec3{ v1.y * v2.z - v1.z * v1.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x };
+	}
+
+	inline float dot(const glm::vec3& v1, const glm::vec3& v2)
+	{
+		return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+	}
+
+	inline glm::vec3 normalize(const glm::vec3 v)
+	{
+		auto mag = sqrt(pow(v.x, 2) + pow(v.y, 2) + pow(v.z, 2));
+
+		glm::vec3 normal = glm::vec3{ v.x / mag, v.y / mag, v.z / mag };
+		return normal;
+	}
+
+	inline glm::vec3 reflect(const glm::vec3& v, const glm::vec3& n)
+	{
+		return glm::reflect(v, n);
 	}
